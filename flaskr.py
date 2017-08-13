fe8704e2e8d85afec21f924c99645dc833b679da
hanjitao <hanjitao2@163.com>
2017-08-13 11:36:33 +0800

    finish db

--- flaskr.py
+++ flaskr.py
@@ -1,39 +1,26 @@
-import os
 import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+from contextlib import closing
+
+# configuration
+DATABASE = 'flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
 
 app = Flask(__name__)
-app.config.from_envvar('FLASKR_SETTINGS',silent=True)
-
+app.config.from_object(__name__)
 def connect_db():
-    """Connects to the specific database.""" 
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
+    return sqlite3.connect(app.config['DATABASE'])
 
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-@app.teardown_appcontext
-def close_db(error):
-    """Closes the database again at the end of the request."""
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
 def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
             db.cursor().executescript(f.read())
         db.commit()
-
-
+        
 if __name__ == '__main__':
     app.run()
-
 
